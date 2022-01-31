3c61779413b005f0c0f44a7ac682a16895d304fb
Sebastian Jennen <sebastian.jennen@gmx.de>
2022-01-31 20:26:25 +0100

    add game entity and show in endpoint

---
+++ database/migrations/2022_01_31_191152_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('participant1_id');
+            $table->unsignedBigInteger('participant2_id');
+            $table->string('result');
+            $table->foreign('participant1_id')->references('id')->on('users');
+            $table->foreign('participant2_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/aaa', function () {
+
+    $game = \App\Models\Game::first();
+    echo 'Player 1:' . $game->participant1_id;
+    echo 'Player 2:' . $game->participant2_id;
+    echo 'result:' . $game->result;
+
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
